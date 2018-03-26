a3a9e2ed5e2dc438ef6e91caab381d3ee4fbbac8
Tauseef Khan <tauseef25@gmail.com>
2018-03-25 17:28:52 -0700

    backing up, new Unity version, SOS

---
+++ Assets/_Scripts/Bounce.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bounce : MonoBehaviour {
+
+	[SerializeField]
+	[Tooltip("Y speed when bouncing off a platform (m/s)")]
+	private float _bounceSpeed = 15.0f;
+
+	private Rigidbody _rb;
+
+	void Start() {
+		_rb = GetComponent<Rigidbody> ();
+	}
+	// Update is called once per frame
+	void OnCollisionEnter (Collision other) {
+		_rb.AddForce (Vector3.up * _bounceSpeed, ForceMode.Impulse);
+	}
+}
---
+++ Assets/_Scripts/NavigationController.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using UnityEngine.AI;
+
+public class NavigationController : MonoBehaviour {
+
+	[SerializeField]
+	private NavMeshAgent agent;
+
+	[SerializeField]
+	private Camera cam;
+
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = cam.ScreenPointToRay (Input.mousePosition);
+			RaycastHit hit;
+
+			if (Physics.Raycast (ray, out hit)) {
+				agent.SetDestination (hit.point);
+			}
+		}
+	}
+}
